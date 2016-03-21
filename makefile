87815abf40b23ab10029d44e5872f1f3106da958
Kovila <k.o.v.i.11.d.a.y@gmail.com>
2016-03-21 16:26:15 +0100

    project backbone

--- makefile
+++ makefile
@@ -4,21 +4,21 @@ YACC=bison -d -v
 LDLIBS=-ll -ly -lm
 CFLAGS=-std=c99 -g -pedantic -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wmissing-prototypes
 
-ytab=web_compiler.tab.c web_compiler.tab.h
+ytab=$(name).tab.c $(name).tab.h
 
-web_compiler: lex.yy.o web_compiler.tab.o struct_forest.o
+$(name): lex.yy.o $(name).tab.o struct_tree.o
 	$(CC) -o $@ $(CFLGS) $^ $(LDLIBS)
 
-lex.yy.c: web_compiler.l web_compiler.tab.h
-	$(LEX) web_compiler.l
+lex.yy.c: $(name).l $(name).tab.h
+	$(LEX) $(name).l
 
-$(ytab): web_compiler.y
+$(ytab): $(name).y
 	$(YACC) $^
 
 .PHONY: clean
 
 clean:
-	rm web_compiler
+	rm $(name)
 	rm -f *.o *~ core
-	rm *.tab.* lex.yy.c *.output
-	rm struct_forest
+	rm *.tab.* lex.yy.* *.output
+
--- struct_tree.h
+++ struct_tree.h
@@ -1,5 +1,6 @@
+#include <stdbool.h>
 
-struct forest
+struct forest;
 struct tree;
 struct attributes;
 
@@ -7,8 +8,8 @@ typedef struct forest* forest;
 typedef struct tree* tree;
 typedef struct attributes* attributes;
 
-enum tree_type {tree, word}; //typage de noeuds: noeud construit d'un arbre ou texte
-enum forest_type {forest, tree};
+//enum tree_type {tree, word}; //typage de noeuds: noeud construit d'un arbre ou texte
+//enum forest_type {forest, tree};
 
 struct attributes{
   char* key; //nom de l'attribut
@@ -18,25 +19,35 @@ struct attributes{
 
 struct tree{
   char* label; //ettiquette du noeud
-  bool nullary; //noeud vide, par exemple <br/> ou type==word
+  bool nullary; //noeud vide, par exemple <br/> 
   bool space; //noeud suivi d'un espace
-  enum tree_type type; //type du noeud. nullary doit etre true si tp vaut word
+  //enum tree_type type; //type du noeud. nullary doit etre true si tp vaut word
   ////tp: feuille: daughter of text node word=feuille empty=feuille
   attributes attr; //arributes du noeud ...list
   tree daughters; //fils gauche, qui doit etre NULL si nullary est true
   tree right; //frere droit
+
+  tree last;
 };
 
 struct forest{
-  enum forest_type type;
-  forest daughters; //NULL if type == tree
+  //enum forest_type type;
+  forest daughters;
   forest right;
-  bool nullary; //foret vide ou type==word
+  bool nullary;
 };
 
 //ajouter struct forest pour pouvoir representer un ficher en entier//
 
-forest create_forest ();
+
+tree create_tree (char* label);
+tree join_to_parent(tree daughter, tree parent);
+tree join_to_siblings(tree new, tree siblings);
+tree create_txt_tree(char* label);
+
+void display_tree(tree t);
+/*
 void destroy_forest ();
 void forest_to_xml (forest f);
 
+*/
